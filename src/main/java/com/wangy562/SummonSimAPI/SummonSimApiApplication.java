3c15a64851df029c8e4750d3a325490d12abed22
wangy562 <andyyqwang@outlook.com>
2022-01-30 00:47:35 -0800

    Create Spring Boot project

---
+++ src/main/java/com/wangy562/SummonSimAPI/SummonSimApiApplication.java
@@ -0,0 +1,13 @@
+package com.wangy562.SummonSimAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummonSimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummonSimApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangy562/SummonSimAPI/SummonSimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangy562.SummonSimAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummonSimApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
