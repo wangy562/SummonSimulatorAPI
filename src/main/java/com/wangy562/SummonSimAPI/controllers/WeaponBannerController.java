67755ca9debe61de0d5dfb3592a8b55352603836
wangy562 <andyyqwang@outlook.com>
2022-02-22 23:38:15 -0800

    add epitomized path functionality

--- src/main/java/com/wangy562/SummonSimAPI/controllers/WeaponBannerController.java
+++ src/main/java/com/wangy562/SummonSimAPI/controllers/WeaponBannerController.java
@@ -1,22 +1,48 @@
 package com.wangy562.SummonSimAPI.controllers;
 
 
-import com.wangy562.SummonSimAPI.SummonItemRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.wangy562.SummonSimAPI.SummonItem;
 import com.wangy562.SummonSimAPI.services.WeaponSummonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/weaponBanner")
 public class WeaponBannerController {
 
-    private SummonItemRepository summonItemRepository;
     private WeaponSummonService weaponSummonService;
 
     @Autowired
-    public WeaponBannerController(SummonItemRepository summonItemRepository, WeaponSummonService weaponSummonService) {
-        this.summonItemRepository = summonItemRepository;
+    public WeaponBannerController(WeaponSummonService weaponSummonService) {
         this.weaponSummonService = weaponSummonService;
+    }
+
+    @GetMapping("/summon")
+    @ResponseBody
+    public SummonItem summonSingle(@RequestBody HashMap<String, Integer> pity) {
+        Integer pityCount = pity.get("pity");
+        Integer epitomized = pity.get("epitomized");  // 0 or 1 or 2for epitomized path
+        return weaponSummonService.summon(pityCount, epitomized);
+    }
+
+    @GetMapping("/multi")
+    @ResponseBody
+    public List<SummonItem> summonMulti(@RequestBody HashMap<String, Integer> pity) {
+        Integer pityCount = pity.get("pity");
+        Integer epitomized = pity.get("epitomized");  // 0 or 1 or 2 for epitomized path
+        List<SummonItem> summonItems = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            summonItems.add(weaponSummonService.summon(pityCount, epitomized));
+            pityCount++;
+        }
+        return summonItems;
     }
 }
--- src/main/java/com/wangy562/SummonSimAPI/services/WeaponSummonService.java
+++ src/main/java/com/wangy562/SummonSimAPI/services/WeaponSummonService.java
@@ -1,6 +1,7 @@
 package com.wangy562.SummonSimAPI.services;
 
 
+import com.wangy562.SummonSimAPI.SummonItem;
 import com.wangy562.SummonSimAPI.SummonItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,15 @@ import org.springframework.stereotype.Service;
 public class WeaponSummonService {
 
     private SummonItemRepository summonItemRepository;
-    private String epitomized;  // after 2 non-epitmoized items, guarantee the item drop
+    private Boolean epitomized;  // after 2 non-epitmoized items, guarantee the item drop
 
     @Autowired
-    public WeaponSummonService(SummonItemRepository summonItemRepository) {
+    public WeaponSummonService(SummonItemRepository summonItemRepository, Integer epitmoized) {
         this.summonItemRepository = summonItemRepository;
-        this.epitomized = "";
+        this.epitomized = epitomized;
+    }
+
+    public SummonItem summon(Integer pityCount, Integer epitomized2) {
+        return null;
     }
 }
