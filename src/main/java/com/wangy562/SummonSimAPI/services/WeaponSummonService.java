36552321b83934f06918be528eb63297ec51da15
wangy562 <andyyqwang@outlook.com>
2022-02-23 15:49:28 -0800

    Fix epitomized bug

--- src/main/java/com/wangy562/SummonSimAPI/services/WeaponSummonService.java
+++ src/main/java/com/wangy562/SummonSimAPI/services/WeaponSummonService.java
@@ -10,15 +10,13 @@ import org.springframework.stereotype.Service;
 public class WeaponSummonService {
 
     private SummonItemRepository summonItemRepository;
-    private Boolean epitomized;  // after 2 non-epitmoized items, guarantee the item drop
 
     @Autowired
-    public WeaponSummonService(SummonItemRepository summonItemRepository, Integer epitmoized) {
+    public WeaponSummonService(SummonItemRepository summonItemRepository) {
         this.summonItemRepository = summonItemRepository;
-        this.epitomized = epitomized;
     }
 
-    public SummonItem summon(Integer pityCount, Integer epitomized2) {
+    public SummonItem summon(Integer pityCount, Integer epitomized) {
         return null;
     }
 }
